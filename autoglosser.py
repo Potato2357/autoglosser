17721cc321de9729daeb265718a2aeff68de6622
Alex Shi <wshi2@swarthmore.edu>
2025-05-02 23:33:00 -0400

    make it work kinda

--- autoglosser.py
+++ autoglosser.py
@@ -1,5 +1,5 @@
 # Usage:
-# echo "your sentence here" | python3 autoglosser.py --igtdef <igt file> --biltrans <directory of bilingual transducer> --morph <directory of monolingual transducer>
+# echo "your sentence here" | python3 autoglosser.py --igtdef <igt file> --biltrans <bilingual transducer binary file> --morph <directory of monolingual transducer>
 #
 #
 #
@@ -24,10 +24,12 @@ gloss = {
     # gender
     "f": ["","F"],
     "m": ["","M"],
+    "mf": ["",""],
     "nt": ["","N"],
     # number
     "sg": ["","SG"],
     "pl": ["","PL"],
+    "sp": ["",""],
     "du": ["","DU"],
     # person
     "p1": ["","1"],
@@ -64,7 +66,12 @@ gloss = {
     "ger": ["","GER"]
 }
 
+puncts = ["<sent>", "<cm>", "<lquot>", "<rquot>", "<lpar>", "<rpar>", "<guio>", "<apos>", "<quot>", "<percent>", "lquest", "clb", "punct"]
+
+
 # read the options
+
+lang = ""
 igtdef = ""
 biltrans = ""
 morph = ""
@@ -83,14 +90,77 @@ while i < len(sys.argv):
     
 if not igtdef or not biltrans or not morph:
     print("ERROR: options not supplied")
+    print("Usage:")
+    print("echo \"your sentence here\" | python3 autoglosser.py --igtdef <igt file> --biltrans <bilingual transducer binary file> --morph <directory of monolingual transducer>")
     sys.exit(1)
 
+# read the sentence to be glossed
 
 input_str = sys.stdin.read()
 input_str = input_str.strip()
-print(input_str)
 
+result = subprocess.run("echo \"" + input_str + "\" | lt-proc " + morph, shell=True, capture_output=True, text=True)
 
-# print(gloss.get("inf")[1])
+words = result.stdout.split("$")
 
-# result = subprocess.run("echo \"" + input_t + "\" | apertium -d . wuu-zho-biltrans", shell=True, capture_output=True, text=True)
+for i in range(len(words)):
+    if words[i].rfind("/") != -1:
+        words[i] = words[i][words[i].rfind("/")+1:]
+
+def is_punctuation(str):
+    for t in puncts:
+        if t in str:
+            return True
+    return False
+
+words = [x for x in words if not is_punctuation(x) and not x == '\n']
+
+words_s = []
+for i in range(len(words)):
+    words_s.append(words[i].split("<"))
+    for j in range(1,len(words_s[i])):
+        words_s[i][j] = words_s[i][j][:-1]
+
+# print(words)
+# print(words_s)
+
+# process stuff
+
+glossed = []
+for i in range(len(words_s)):
+    glossed_word = ""
+    for j in range(len(words_s[i])):
+        if j == 0:
+            result = subprocess.run("echo \"" + words[i] + "\" | lt-proc " + biltrans, shell=True, capture_output=True, text=True)
+            temp = result.stdout[result.stdout.find("/")+1:]
+            if temp.find("<") != -1:
+                temp = temp[:temp.find("<")]
+                glossed_word = temp.replace(" ", "-")
+            else:
+                glossed_word = "<" + words_s[i][0] + ">"
+        else:
+            if words_s[i][j] in gloss:
+                if gloss.get(words_s[i][j])[0]:
+                    glossed_word = gloss.get(words_s[i][j])[0] + "." + glossed_word
+                if gloss.get(words_s[i][j])[1]:
+                    glossed_word = glossed_word + "." + gloss.get(words_s[i][j])[1]
+    #fix some stuff
+    glossed_word = glossed_word.replace("1.SG", "1SG")
+    glossed_word = glossed_word.replace("2.SG", "2SG")
+    glossed_word = glossed_word.replace("3.SG", "3SG")
+    glossed_word = glossed_word.replace("1.PL", "1PL")
+    glossed_word = glossed_word.replace("2.PL", "2PL")
+    glossed_word = glossed_word.replace("3.PL", "3PL")
+    glossed.append(glossed_word)
+
+# output
+
+for i in range(len(words_s)):
+    print(words_s[i][0] + " ", end='')
+
+print()
+
+for i in range(len(words_s)):
+    print(glossed[i] + " ", end='')
+
+print()
